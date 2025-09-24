04645e381da0b93f8168aff147b30e3266a5931e
Michel <98588393+Maikel2812@users.noreply.github.com>
2025-09-24 20:41:00 +0700

    24 September

--- routes/web.js
+++ routes/web.js
@@ -7,8 +7,13 @@ web.get('/',(req,res)=>{
 })
 
 web.get('/:username',(req,res)=>{
-    res.render('public-profile')
+    const username = req.params.username
+    res.render('public-profile',{
+        title : username,
+        username : username,
+        bio : "Hi Iscarions"
 
+    })
 })
 
 export default web
