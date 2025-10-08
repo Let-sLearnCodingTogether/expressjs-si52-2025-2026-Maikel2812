a0a6aba77f581b5c89d8d049f22222ae9f8c1157
Michel <98588393+Maikel2812@users.noreply.github.com>
2025-10-08 20:39:58 +0700

    8 Oktober (2)

--- controller/authController.js
+++ controller/authController.js
@@ -1,27 +1,58 @@
 import UserModel from "../models/userModel.js";
-
-export const register = async (req, res) => {
+export const register = async (req, res) => {}
     try {
-        //Untuk mengambil body atau data dari request
-        const registerData = req.body
-
+        const registerData=req.body
         console.log(registerData);
+
+        const hashPassword = hash(registerData.password)
 
         await UserModel.create({
             username : registerData.username,
             email : registerData.email,
             password : registerData.password
         })
-
         res.status(201).json ({
-            message : "Berhasil register, silahkan login",
+            massage : "Berhasil register, silahkan login",
             data : null
         })
     }
     catch(e) {
-        res.status(500).json ({
-            message : e.message,
+        es.status(500).json ({
+            massage : e.massage,
             data : null
-        })
+    })
     }
-}
+    export const login = async(req,res)=>{
+        try{
+            const loginData = req.body
+            const user = await UserModel.findOne({
+                email: loginData.email
+            })
+            if (!user){
+                res.status(404).json({
+                    message: "User tidak di temukan",
+                    data:null
+                })
+            }
+            if(user.password == loginData.password){
+                return res.status(200).json({
+                    message : "Login Berhasil",
+                    data : {
+                        username : user.username,
+                        email : user.email,
+                        token : "TOKEN"
+                    }
+                })
+            }
+            return res.status(401).json({
+                message : "Login gagal",
+                data : null
+            })
+
+        } catch {error}{
+            res.status(500).json({
+                message: error,
+                data : null
+            })
+        }
+    }
