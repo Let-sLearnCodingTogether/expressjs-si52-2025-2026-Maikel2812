6223915f39be871ef9e15b531268b6bae7f8857b
Michel <98588393+Maikel2812@users.noreply.github.com>
2025-10-25 17:27:21 +0700

    25 Oktober

--- config/passport.js
+++ config/passport.js
@@ -1,32 +1,33 @@
 import passport from "passport";
-
-import{ExtractJwt,Strategy} from "passport-jwt"
+import {ExtractJwt, Strategy} from "passport-jwt"
 import UserModel from "../models/userModel.js";
 
-const opts={
-    jwtFromRequest:ExtractJwt.fromAuthHeaderAsBearerToken(),
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey : "JWT_SECRET_KEY"
 }
 
 passport.use(
-    new Strategy(opts,async(payload,done)=>{
+    new Strategy(opts, async (payload, done) => {
         try{
-            const user = await UserModel.findById({
+            //Melakukan query kedalam model user berdasarkan email
+            const user = await UserModel.findOne({
                 email : payload.email
             })
 
-            if(!user){
+            //Jika user tidak di temukan
+            if(!user) {
                 return done(null,false)
             }
-            return done(null,{
+
+            //Jika user ditemukan masukkan payload berikut ini.
+            return done(null, {
                 id : user._id,
                 email : user.email,
                 username : user.username
             })
-        }catch(error){
-            return done(null,false)
-        }   
+        } catch (error) {
+            return done (null,false)
+        }
     })
 )
-
-export default passport
--- controller/authController.js
+++ controller/authController.js
@@ -1,73 +1,73 @@
+import {compare, hash} from "../utils/hashUtil.js";
 import UserModel from "../models/userModel.js";
-import {compare,hash} from "../utils/hashUtil.js";
 import { jwtSignUtil } from "../utils/jwtSignUtil.js";
+import { request } from "express";
+
 export const register = async (req, res) => {
     try {
-        const registerData=req.body
-        console.log(registerData);
+        //Untuk mengambil body atau data dari request
+        const request = req.body
+        console.log(request);
 
-        const hashPassword = hash(registerData.password)
+        const hashPassword =await hash(request.password)
 
         await UserModel.create({
-            username : registerData.username,
-            email : registerData.email,
-            password : registerData.password
+            username : request.username,
+            email : request.email,
+            password : hashPassword
         })
+
         res.status(201).json ({
-            massage : "Berhasil register, silahkan login",
+            message : "Berhasil register, silahkan login",
             data : null
         })
     }
     catch(e) {
         res.status(500).json ({
-            massage : e.massage,
+            message : e.message,
             data : null
-    })
+        })
     }
 }
-    export const login = async(req,res)=>{
-        try{
-            const loginData = req.body
-            const user = await UserModel.findOne({
-                email: loginData.email
-            })
-            if (!user){
-                res.status(404).json({
-                    message: "User tidak di temukan",
-                    data:null
-                })
-            }
-            if(user.password == loginData.password){
-                return res.status(200).json({
-                    message : "Login Berhasil",
-                    data : {
-                        username : user.username,
-                        email : user.email,
-                        token : "TOKEN"
-                    }
-                })
-            }
 
-            if(compare(loginData.password, user.password)){
-                return res.status(200).json({
-                    message : "Login Berhasil",
-                    data : {
-                        username : user.username,
-                        email : user.email,
-                        token : jwtSignUtil(user)
-                    }
-                })
-            }
-            return res.status(401).json({
-                message : "Login gagal",
-                data : null
-            })
+export const login = async (req,res) => {
+    try {
+        const request = req.body
+        console.log(request);
 
-        } catch {error}{
-            res.status(500).json({
-                message: error,
-                data : null
+        //Mencari user berdasarkan email
+        const user = await UserModel.findOne({
+            email: request.email
+        })
+        const isPasswordMatch = compare(request.password, user.password);
+        //Jika user tidak ditemukan
+        if(!isPasswordMatch) {
+            res.status(404).json ({
+                message: "User tidak ditemukan",
+                data: null
             })
         }
-    }
 
+        //membandingkan password yang ada di dalam db dengan request
+        if(await compare(request.password, user.password)){
+            return res.status(200).json({
+                message: "Login berhasil",
+                data: {
+                    username: user.username,
+                    email: user.email,
+                    token: jwtSignUtil(user) //Melakukan sign JWT Token
+                }
+            })
+        }
+        return res.status(401).json({
+                message: "Login gagal",
+                data: null
+            })
+        
+    } catch (error) {
+        res.status(500).json({
+            message: error,
+            data: null
+        })
+    }
+}
